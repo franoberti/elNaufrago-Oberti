1d3b39e669180feb406131e1f4705033afb98cdd
Francisco Oberti <franoberti45@gmail.com>
2022-08-17 21:28:22 -0300

    Agrega Prductos con Contador Incorporado

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <Navbar/>
       <ItemListContainer greeting={"Bienvenido a la Librería El Náufrago"}/>
+      
+      
     </div>    
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import ItemCount from '../../components/ItemCount/ItemCount'
+import productos from '../../products/products.json'
 
 const ItemListContainer = ({greeting}) => {
   return (
+    <>
     <h1 className='titulo p-5'>
         {greeting}
     </h1>
+    <div class="container">
+      <div class="row">
+        {productos.map((item, index)=>(
+          <ItemCount key={index} title={item.title} initial={item.initial} image={item.image} stock={item.stock}/>
+        ))} 
+      </div>
+    </div>
+  </>
   )
 }
 
